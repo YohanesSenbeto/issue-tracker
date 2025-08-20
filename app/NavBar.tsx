25090ac99c150fead17c39131861209291c6260e
Yohanes Senbeto <121575345+YohanesSenbeto@users.noreply.github.com>
2025-08-20 12:39:25 +0300

    Added usepathname and css configured for navbars

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,14 +1,37 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import { AiFillBug } from "react-icons/ai";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
+    const currentPath = usePathname();
+    console.log(CurrentPath);
+
+    const Links = [
+        { name: "Dashboard", href: "/" },
+        { name: "Issues", href: "/issues" },
+    ];
     return (
         <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
             <Link className="text-xl" href="/">
                 <AiFillBug />
             </Link>
             <ul className="flex space-x-6">
+                {Links.map((link) => (
+                    <li key={link.href}>
+                        <Link
+                            className={`${
+                                currentPath === link.href
+                                    ? "text-zinc-900 font-semibold"
+                                    : "text-zinc-500 hover:text-zinc-800 transition-colors"
+                            }`}
+                            href={link.href}
+                        >
+                            {link.name}
+                        </Link>
+                    </li>
+                ))}
                 <li>
                     <Link
                         className="text-zinc-500 hover:text-zinc-800 transition-colors"
